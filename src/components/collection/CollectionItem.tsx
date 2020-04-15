9a81eb5458372c5fc48d28ecd3dfac6e7d93fc39
Elias Jackson <ejthedj0@gmail.com>
2020-04-15 16:53:55 -0700

    Album art and title display

--- src/components/collection/CollectionItem.tsx
+++ src/components/collection/CollectionItem.tsx
@@ -1,5 +1,11 @@
 import React, { ReactElement } from 'react';
 import { Paper, makeStyles, createStyles, Theme, Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 import { getCollection_getUser_collection } from '../../../__generated__/getCollection';
 
 type CollectionItem = Omit<getCollection_getUser_collection, '__typename'>;
@@ -13,10 +19,12 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+      width:200,
     },
-    paper: {
-      height: 140,
-      width: 140,
+    itemText: {
+      textOverflow: 'ellipsis',
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
       textAlign: 'center'
     },
     control: {
@@ -28,8 +36,28 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function CollectionItemComponent({item}: Props): ReactElement {
   const classes = useStyles();
   return (
-    <Paper className={classes.paper}>
-      <Typography>{item.artist} - {item.title}</Typography>
-    </Paper>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={`${item.title}`}
+          height="200"
+          image={`https://coverartarchive.org/release-group/${item.mbid}/front-250`}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography className={classes.itemText} gutterBottom>{item.artist} </Typography>
+          <Typography className={classes.itemText} gutterBottom>{item.title} </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
