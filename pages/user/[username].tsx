93617b0d2ef6c43dbc72ffcfb935f8c045253e66
Elias Jackson <ejthedj0@gmail.com>
2020-04-15 20:45:52 -0700

    Re-added Apollo in main page

--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -1,4 +1,9 @@
 import React, { ReactElement } from 'react';
+import { useRouter } from 'next/router'
+import PropTypes from 'prop-types'
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+
 import CollectionComponent from '../../src/components/collection/Collection';
 import collectionMockData from '../../data/mock-data-1.json';
 
@@ -7,9 +12,36 @@ interface Props {
 }
 
 export default function UserPage({}: Props): ReactElement {
+  const router = useRouter();
+  const { username } = router.query;
+
+  const GET_COLLECTION_QUERY = gql`
+    query getCollection {
+      getUser(username: "user1") {
+        username
+        collection {
+          artist
+          albumName as title
+        }
+      }
+    }
+  `;
+
+  let collection: any;
+  let loading, error, data;
+  if(username === 'mock') {
+    collection = collectionMockData;
+    loading = error = false;
+  }
+  else {
+    ({ loading, error, data } = useQuery(GET_COLLECTION_QUERY));
+    collection = data.collection
+  }
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
   return (
     <div>
-      <CollectionComponent collection={collectionMockData}></CollectionComponent>
+      <CollectionComponent collection={collection}></CollectionComponent>
     </div>
   );
 }
