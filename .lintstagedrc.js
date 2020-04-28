77e0cc7c9bf4f38eb866c00d1dad2dbc89606084
Elias Jackson <ejthedj0@gmail.com>
2020-04-27 19:45:44 -0700

    Testing another docs change

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'{lib,pages,src}/*.ts': ['eslint --fix', 'prettier --write'],
 	'{lib,pages,src}/*.{js,json}': ['prettier --write'],
-	'docs/**/*': () => 'mkdocs build && git add public/docs',
+	'docs/**/*': () => ['mkdocs build', 'git add public/docs'],
 };
